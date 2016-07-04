32b38c8780c4c8910a3d34f736d2e181f2f3efe3
Dillon Jason <Dillon Jason>
2016-07-04 16:09:21 -0400

    Added content component

--- app/views/education.jsx
+++ app/views/education.jsx
@@ -1,4 +1,6 @@
 import SummaryContainer from '../components/summary-container'
+import Quote from '../components/quote'
+import Content from '../components/content'
 import EducationImage from '../images/education-image.jpg'
 
 class Education extends React.Component {
@@ -6,12 +8,24 @@ class Education extends React.Component {
         return(
             <div>
                 <SummaryContainer graphic={EducationImage}>
-                    Some education stuffs
+                    <Quote
+                        quote="I am always doing that which I cannot do, in order that I may learn how to do it."
+                        source="Pablo Picasso"
+                    />
                 </SummaryContainer>
 
-                <div className="content">
-                    Some crazy stuff is gonna happen here.
-                </div>
+                <Content
+                    contentClass={true}
+                    title="Virginia Commonwealth University"
+                    date="July 2016"
+                    subTitle="Richmond, VA"
+                >
+                    <p>
+                        Graduated August 2012<br/>
+                        Bachelor of Science, Computer Science<br/>
+                        Major GPA: 3.25     GPA: 2.85
+                    </p>
+                </Content>
             </div>
         );
     }
