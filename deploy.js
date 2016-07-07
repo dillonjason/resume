fb76a4c29fbbc9a46e5c44d37e703e1343633cc8
Dillon Jason <Dillon Jason>
2016-07-07 19:12:33 -0400

    added deploy?

---
+++ deploy.js
@@ -0,0 +1,8 @@
+var child_process = require('child_process');
+child_process.exec("webpack", function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+});
