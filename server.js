409d0e32dfe29018f80f556db71a8d60e5a637f6
Dillon Jason <Dillon Jason>
2016-07-09 13:25:43 -0400

    amde compress the first extension used on server

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ var app = express();
 var port = (process.env.PORT || 8080);
 var publicPath = path.resolve(__dirname, 'build');
 
+// Must be first
+app.use(compression());
+
 // We point to our static assets
 app.use(express.static(publicPath));
-app.use(compression());
+
 
 // And run the server
 app.listen(port, function () {
