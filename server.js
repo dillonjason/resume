b10c85a96630d3c6fb0e2bc6fbbf2f6dbca01a6d
Dillon Jason <Dillon Jason>
2016-07-09 12:44:44 -0400

    Added compression to server

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var path = require('path');
+var compression = require('compression');
 
 var app = express();
 
-var port = process.env.PORT;
+var port = (process.env.PORT || 8080);
 var publicPath = path.resolve(__dirname, 'build');
 
 // We point to our static assets
 app.use(express.static(publicPath));
+app.use(compression());
 
 // And run the server
 app.listen(port, function () {
