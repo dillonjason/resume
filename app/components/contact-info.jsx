050bedff233093cd5f499cab0dd76ef79a570d35
Dillon Jason <Dillon Jason>
2016-07-10 11:48:56 -0400

    Added about me information

--- app/components/contact-info.jsx
+++ app/components/contact-info.jsx
@@ -5,13 +5,45 @@ class ContactInfo extends React.Component {
     render() {
         return(
             <Modal
-                title="Contact Me"
+                title="About Me"
                 graphic={MyPicture}
                 onCloseClick={this.props.onCloseClick}
                 show={this.props.show}
             >
                 <p>
-                    Fill this in with a little info about me.
+                    Welcome to my webpage!<br/>
+                    My name is Dillon Jason, a full stack developer with a slight bit more interest in front end work.
+                </p>
+
+                <p>
+                    I am ver passionate about my work and strive to continue learning new and exciting things in the
+                    field of web development.  I have recently become very impressed with a React, Redux, Webpack, SASS, and
+                    PostCSS development stack.  I believe that code should be simple to read and follow and modularized
+                    for increased reusability.  There are many different design principles out there, but I find these two
+                    concepts to be the primary foundation for almost all of them.
+                </p>
+
+                <p>
+                    In my free time I enjoy working on new projects, playing hours of video games, and getting excited
+                    about new gadgets and gizmos.
+                </p>
+
+                <p>
+                    In the past year I have been exposed to a lot of new technologies but haven't always had the time at work to try them out,
+                    so I often find myself working on little side projects.  Most recently I have worked on refining my knowledge of
+                    React and Webpack with this site!
+                </p>
+
+                <p>
+                    The first thing I did after getting a "real" job out of college was build a gaming PC.  I have put an unhealthy amount of hours
+                    in the Dark Souls series, Skyrim, Civilization, Fallout, and many more.  I just picked up Stellaris and Overwatch
+                    and am excited to see ho fun they are!  Feel free to send me any email or friend me on Steam (djasuar)!
+                </p>
+
+                <p>
+                    I'm a pretty big fan of Android, so I like to keep up on all the stuff they release (Google I/O is like Christmas to me).
+                    I think it's incredible how dramatically technology and the internet can change our lives so I love to get all the latest
+                    info on all the new and incredbile things coming out.
                 </p>
 
                 <div className="pure-g">
