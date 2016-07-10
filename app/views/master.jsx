76f928b56958cd7361a962d4e4d0cdb5c879af36
Dillon Jason <Dillon Jason>
2016-07-09 21:09:41 -0400

    Added delay to scroll up for transition animation.  Tab title to ma name

--- app/views/master.jsx
+++ app/views/master.jsx
@@ -3,76 +3,53 @@ import Navbar from '../components/nav/navbar'
 import { browserHistory } from 'react-router'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
-export default ({children, location}) => {
-    window.scrollTo(0,0);
-    let isHome = location.pathname === '/';
+class Master extends React.Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <div>
-            <Navbar
-                header="DILLON JASON"
-                navItems={
-                    [
-                        {name: "Education", to: "/Education"},
-                        {name: "Experience", to: "/Experience"},
-                        {name: "Skills", to: "/Skills"},
-                        {name: "Achievements", to: "/Achievements"}
-                    ]
-                }
-                showCards={isHome}
-            />
-            <ReactCSSTransitionGroup
-                component="div"
-                className="react-animate-main"
-                transitionName="main-animate"
-                transitionEnterTimeout={1500}
-                transitionLeaveTimeout={1500}
-            >
-                {React.cloneElement(children, {
-                    key: location.pathname
-                })}
-            </ReactCSSTransitionGroup>
-        </div>
-    );
+        this.state = {
+            scrollDelay: null
+        };
+    }
+    componentWillUpdate() {
+        if (this.state.scrollDelay)
+            window.clearTimeout(this.state.scrollDelay);
+
+        this.state.scrollDelay = window.setTimeout(() => {
+            window.scrollTo(0,0);
+        }, 750);
+    }
+
+    render() {
+        let isHome = this.props.location.pathname === '/';
+        return (
+            <div>
+                <Navbar
+                    header="DILLON JASON"
+                    navItems={
+                        [
+                            {name: "Education", to: "/Education"},
+                            {name: "Experience", to: "/Experience"},
+                            {name: "Skills", to: "/Skills"},
+                            {name: "Achievements", to: "/Achievements"}
+                        ]
+                    }
+                    showCards={isHome}
+                />
+                <ReactCSSTransitionGroup
+                    component="div"
+                    className="react-animate-main"
+                    transitionName="main-animate"
+                    transitionEnterTimeout={1500}
+                    transitionLeaveTimeout={1500}
+                >
+                    {React.cloneElement(this.props.children, {
+                        key: this.props.location.pathname
+                    })}
+                </ReactCSSTransitionGroup>
+            </div>
+        );
+    }
 }
 
-// class Master extends React.Component {
-//     componentWillUpdate() {
-//         window.scrollTo(0,0);
-//     }
-//
-//     render() {
-//         let isHome = this.props.location.pathname === '/';
-//         return (
-//             <div>
-//                 <Navbar
-//                     header="DILLON JASON"
-//                     navItems={
-//                         [
-//                             {name: "Education", to: "/Education"},
-//                             {name: "Experience", to: "/Experience"},
-//                             {name: "Skills", to: "/Skills"},
-//                             {name: "Achievements", to: "/Achievements"}
-//                         ]
-//                     }
-//                     showCards={isHome}
-//                 />
-//                 <ReactCSSTransitionGroup
-//                     component="div"
-//                     className="react-animate-main"
-//                     transitionName="main-animate"
-//                     transitionEnterTimeout={750}
-//                     transitionLeaveTimeout={750}
-//                 >
-//                     <View className={isHome ? "menu-not-fixed" : ""}>
-//                         {React.cloneElement(this.props.children, {
-//                             key: this.props.location.pathname
-//                         })}
-//                     </View>
-//                 </ReactCSSTransitionGroup>
-//             </div>
-//         );
-//     }
-// }
-//
-// export default Master;
+export default Master;
